92b853a3bbad086bd280dd51a10eceba3828bf48
Martin Killmann <martin.killmann@gmail.com>
2026-09-24 01:47:17 +0900

    Sync 7 pack(s) + setup.js from 13a-rules-db; bump to 0.1.6

--- scripts/setup.js
+++ scripts/setup.js
@@ -106,7 +106,7 @@ Hooks.on("ready", function () {
   CONFIG.ARCHMAGE.keyModifiers["savage"]["rogue"] = ["str","dex"];
   CONFIG.ARCHMAGE.keyModifiers["savage"]["sorcerer"] = ["str","cha"];
   CONFIG.ARCHMAGE.keyModifiers["savage"]["wizard"] = ["int","str"];
-  CONFIG.ARCHMAGE.classResources["savage"] = [["Frenzydice","quickreset"],["Frenzydiesize","none"]];
+  CONFIG.ARCHMAGE.classResources["savage"] = [["Frenzy","quickreset"],["Frenzydiesize","none"]];
   console.log("Savage (13th Age) class loaded successfully.");
 
   console.log("Swordmage (13th Age) class injecting data into the system...");
